294e178767778c505534acbb3b68fef3db391c64
AbdelaliChe <chentouiabdelali@gmail.com>
2023-12-31 15:51:07 +0100

    update readme

--- my_pharmacy/__manifest__.py
+++ my_pharmacy/__manifest__.py
@@ -6,8 +6,8 @@
     'description': 'This module allows you to manage pharmacy permanence and proximite.',
     'depends': ['base', 'website'],
     'data': [
-        'security/ir.model.access.csv',
         'security/security.xml',
+        'security/ir.model.access.csv',
         'views/pharmacy_view.xml',
         'views/pharmacy_template.xml',
         'views/pharmacy_detail_template.xml',
